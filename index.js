87c3025e8d420f17fb354f41f387bd12ded8a5d2
Vaibhav Badaya <153429925+vaibhavbadaya@users.noreply.github.com>
2026-01-05 15:35:15 +0530

    Added Chnages and fixed some bugs.

--- index.js
+++ index.js
@@ -3,17 +3,29 @@ const app = express();
 const PORT = 8000;
 const path = require("path");
 const userRouter = require("./routers/user");
+const mongoose = require("mongoose");
 
-app.use(express.urlencoded({extended:true}));
+
+mongoose.connect('mongodb://localhost:27017/BlogTech').then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Error connecting to MongoDB", err);
+});
+
+
 app.use(express.json());
-
+app.use(express.urlencoded({extended:true}));
 app.use("/user", userRouter);
+
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- models/user.js
+++ models/user.js
@@ -1,34 +1,31 @@
 const {Schema,model} = require('mongoose');
-const {Hmac,randomBytes} = require('crypto');
+const {createHmac,randomBytes} = require('crypto');
 
 
 const userSchema = new Schema({
     fullName: {type: String, required: true},
     email: {type: String, required: true, unique: true},
-    salt: {type: String, required: true},
+    salt: {type: String},
     password: {type: String, required: true},
     profileImageURL: {type: String, default: '/images/default.png'},
     role: {type: String, enum: ['user', 'admin'], default: 'user'}
     
 }, { timestamps: true });
 
-userSchema.pre('save', function(next) {
+userSchema.pre('save', async function() {
     const user = this;
 
     if (!user.isModified('password')) {
-        return
+        return;
     }
 
-    const salt = randomBytes(16).toString();
-    const hashedPassword = this.createHmac('sha256', salt)
-    .update(user.password)
-    .digest('hex');
+    const salt = randomBytes(16).toString('hex');
+    const hashedPassword = createHmac('sha256', salt).update(user.password).digest('hex');
 
     this.salt = salt;
     this.password = hashedPassword;
-    next();
 })
 
-const User = model('User', userSchema);
+const User = model('user', userSchema);
 
 module.exports = User;
